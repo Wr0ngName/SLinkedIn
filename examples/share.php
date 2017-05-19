b357e859743f555c947726a7cb8ce7acb77b7e95
Florent <Wr0ngName@users.noreply.github.com>
2017-05-19 13:07:23 +0200

    Updated JSON data post

--- examples/share.php
+++ examples/share.php
@@ -3,21 +3,29 @@
 include '../simplelinkedin.class.php';
 
 //Set up the API
-$ln = new SimpleLinkedIn('YOUR_API_KEY', 'YOUR_API_SECRET');
-$ln->addScope('rw_nus');
+$ln = new SimpleLinkedIn('YOUR_API_KEY', 'YOUR_API_SECRET', null, array('w_share'));
+
 //Authorize.
 if($ln->authorize()){
-    //Make an example post, and print the result with print_r
-    print_r ($ln->fetch('POST','/v1/people/~/shares?format=json',
-        array(
-            'comment' => 'Hello Linkedin',
-            'content' => array(
-                'title' => 'SimpleLinkedIn (SLinkedin)',
-                'description' => 'Open source OAuth2 Implementation for PHP and linkedin.',
-                'submittedUrl' => 'https://github.com/Wr0ngName/SLinkedIn/'
-            ),
-            'visibility' => array('code' => 'anyone' )
+    //Make an example post
+
+    try
+    {
+        $ln->fetch('POST','/v1/people/~/shares?format=json',
+            array(
+                'comment' => 'Hello Linkedin',
+                'content' => array(
+                    'title' => 'SimpleLinkedIn (SLinkedin)',
+                    'description' => 'Open source OAuth2 Implementation for PHP and linkedin.',
+                    'submittedUrl' => 'https://github.com/Wr0ngName/SLinkedIn/'
+                ),
+                'visibility' => array('code' => 'anyone' )
+            )
         )
-    ));
+    }
+    catch(Exception $e)
+    {
+        echo 'Error:'.$e->getMessage();
+    }
 }
 ?>
