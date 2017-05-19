9a233db09ac4e729409e7a8e0e74683d4426a134
Florent <Wr0ngName@users.noreply.github.com>
2017-05-19 11:27:16 +0200

    Change target URL with corresponding format args
    Creates a bug otherwise : "Error in parsing: Unexpected character '0'"...

--- examples/share.php
+++ examples/share.php
@@ -8,13 +8,13 @@ $ln->addScope('rw_nus');
 //Authorize.
 if($ln->authorize()){
     //Make an example post, and print the result with print_r
-    print_r ($ln->fetch('POST','/v1/people/~/shares',
+    print_r ($ln->fetch('POST','/v1/people/~/shares?format=json',
         array(
             'comment' => 'Hello Linkedin',
             'content' => array(
                 'title' => 'SimpleLinkedIn (SLinkedin)',
                 'description' => 'Open source OAuth2 Implementation for PHP and linkedin.',
-                'submittedUrl' => 'https://github.com/EJTH/SLinkedIn/'
+                'submittedUrl' => 'https://github.com/Wr0ngName/SLinkedIn/'
             ),
             'visibility' => array('code' => 'anyone' )
         )
